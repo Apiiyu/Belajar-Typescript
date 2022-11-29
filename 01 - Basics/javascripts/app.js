7c179c6fd560c6327a33b8b0db4e8ebd3c325689
Apiiyu <khoirulloh.rafi2@gmail.com>
2022-11-29 13:06:36 +0700

    Learning: Union Types on Typescript

--- 01 - Basics/javascripts/app.js
+++ 01 - Basics/javascripts/app.js
@@ -1,32 +1,10 @@
-var roleUsers;
-(function (roleUsers) {
-    roleUsers[roleUsers["FRONTEND"] = 0] = "FRONTEND";
-    roleUsers[roleUsers["BACKEND"] = 1] = "BACKEND";
-    roleUsers[roleUsers["FULLSTACK"] = 2] = "FULLSTACK";
-})(roleUsers || (roleUsers = {}));
-var exampleDataUsers = {
-    name: "Rafi",
-    age: 18,
-    country: "Indonesia",
-    hobbies: ["Coding", "Reading", "Gaming"],
-    someMixedArray: ["Coding", 18, "Indonesia"],
-    someFixedArray: ["Tuple Types", 2],
-    isAlive: true,
-    role: roleUsers.FULLSTACK
+var combineValue = function (firstParameter, secondParameter) {
+    if (typeof firstParameter === 'number' && typeof secondParameter === 'number') {
+        return firstParameter + secondParameter;
+    }
+    else {
+        return firstParameter + '' + secondParameter + '';
+    }
 };
-exampleDataUsers.someFixedArray.push("Rafi"); // --> In Tuple types, Typescript will allowed us to push new data to the array, but it will throw an error if we try to push a new data with different type. Because in typescript push is exception for Tuple types
-exampleDataUsers.someFixedArray[1] = 10; // --> We can also change the value of the array
-// exampleDataUsers.someFixedArray = ["Rafi", 18, 19] // --> But we can't change the length of the array, because it's fixed but you should remember for method push is exception for Tuple types
-console.log(exampleDataUsers.age);
-console.log(exampleDataUsers);
-for (var _i = 0, _a = exampleDataUsers.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
-}
-for (var _b = 0, _c = exampleDataUsers.someMixedArray; _b < _c.length; _b++) {
-    var mixed = _c[_b];
-    console.log(mixed);
-}
-if (exampleDataUsers.role === roleUsers.FULLSTACK) {
-    console.log("Amazing! You are a Fullstack Developer!");
-}
+console.log(combineValue(1, 2));
+console.log(combineValue('Rafi', 'Khoirulloh'));
--- 01 - Basics/javascripts/app.ts
+++ 01 - Basics/javascripts/app.ts
@@ -1,45 +1,16 @@
-enum roleUsers { // --> In Typescript we can use enums to define a set of named constants
-  FRONTEND,
-  BACKEND,
-  FULLSTACK
+/**
+ * @description Handle function combine value to try about Union Types
+ * @param {Number | String} firstParameter 
+ * @param {Number | String} secondParameter 
+ * @returns 
+ */
+const combineValue = (firstParameter: number | string, secondParameter: number | string) => { // --> This paramters can be number or string, and it usually called Union Types
+  if (typeof firstParameter === 'number' && typeof secondParameter === 'number') {
+    return firstParameter + secondParameter;
+  } else {
+    return firstParameter+'' + secondParameter+'';
+  }
 }
 
-type IUsers = { // --> First, we define the type of the data we want to store
-  name: string;
-  age: number;
-  country: string;
-  hobbies: string[]; // --> We can also define arrays like this
-  someMixedArray: (string | number)[]; // --> We can also define arrays with mixed types like this, and it also called as union types
-  someFixedArray: [string, number]; // --> We can also define arrays with fixed types and length like this, and it also called as Tuple
-  isAlive: boolean;
-  role: roleUsers;  
-}
-
-const exampleDataUsers:IUsers = { // --> Then, we create a variable of that type and assign it some data
-  name: "Rafi",
-  age: 18,
-  country: "Indonesia",
-  hobbies: ["Coding", "Reading", "Gaming"],
-  someMixedArray: ["Coding", 18, "Indonesia"],
-  someFixedArray: ["Tuple Types", 2],
-  isAlive: true,
-  role: roleUsers.FULLSTACK
-}
-
-exampleDataUsers.someFixedArray.push("Rafi"); // --> In Tuple types, Typescript will allowed us to push new data to the array, but it will throw an error if we try to push a new data with different type. Because in typescript push is exception for Tuple types
-exampleDataUsers.someFixedArray[1] = 10 // --> We can also change the value of the array
-// exampleDataUsers.someFixedArray = ["Rafi", 18, 19] // --> But we can't change the length of the array, because it's fixed but you should remember for method push is exception for Tuple types
-console.log(exampleDataUsers.age)
-console.log(exampleDataUsers);
-
-for (let hobby of exampleDataUsers.hobbies) {
-  console.log(hobby);
-}
-
-for (let mixed of exampleDataUsers.someMixedArray) {
-  console.log(mixed);
-}
-
-if (exampleDataUsers.role === roleUsers.FULLSTACK) {
-  console.log("Amazing! You are a Fullstack Developer!");
-}
+console.log(combineValue(1, 2));
+console.log(combineValue('Rafi', 'Khoirulloh'));
