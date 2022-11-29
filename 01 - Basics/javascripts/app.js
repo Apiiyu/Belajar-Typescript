b973ffef7c1766d213f42ce03fc67d0aba0a3b99
Apiiyu <khoirulloh.rafi2@gmail.com>
2022-11-29 12:01:59 +0700

    Learning: Arrays Types on Typescript

--- 01 - Basics/javascripts/app.js
+++ 01 - Basics/javascripts/app.js
@@ -2,7 +2,17 @@ var exampleDataUsers = {
     name: "Rafi",
     age: 18,
     country: "Indonesia",
+    hobbies: ["Coding", "Reading", "Gaming"],
+    someMixedArray: ["Coding", 18, "Indonesia"],
     isAlive: true
 };
 console.log(exampleDataUsers.age);
 console.log(exampleDataUsers);
+for (var _i = 0, _a = exampleDataUsers.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
+for (var _b = 0, _c = exampleDataUsers.someMixedArray; _b < _c.length; _b++) {
+    var mixed = _c[_b];
+    console.log(mixed);
+}
--- 01 - Basics/javascripts/app.ts
+++ 01 - Basics/javascripts/app.ts
@@ -2,6 +2,8 @@ type IUsers = { // --> First, we define the type of the data we want to store
   name: string;
   age: number;
   country: string;
+  hobbies: string[]; // --> We can also define arrays like this
+  someMixedArray: (string | number)[]; // --> We can also define arrays with mixed types like this
   isAlive: boolean;  
 }
 
@@ -9,8 +11,18 @@ const exampleDataUsers:IUsers = { // --> Then, we create a variable of that type
   name: "Rafi",
   age: 18,
   country: "Indonesia",
+  hobbies: ["Coding", "Reading", "Gaming"],
+  someMixedArray: ["Coding", 18, "Indonesia"],
   isAlive: true
 }
 
 console.log(exampleDataUsers.age)
 console.log(exampleDataUsers);
+
+for (let hobby of exampleDataUsers.hobbies) {
+  console.log(hobby);
+}
+
+for (let mixed of exampleDataUsers.someMixedArray) {
+  console.log(mixed);
+}
