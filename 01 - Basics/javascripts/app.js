c57fe383c7cf5106d2fbf553cc635d33ccd4ec30
Apiiyu <khoirulloh.rafi2@gmail.com>
2022-11-29 12:20:42 +0700

    Learning: Tuples on Typescript

--- 01 - Basics/javascripts/app.js
+++ 01 - Basics/javascripts/app.js
@@ -4,8 +4,11 @@ var exampleDataUsers = {
     country: "Indonesia",
     hobbies: ["Coding", "Reading", "Gaming"],
     someMixedArray: ["Coding", 18, "Indonesia"],
+    someFixedArray: ["Tuple Types", 2],
     isAlive: true
 };
+exampleDataUsers.someFixedArray.push("Rafi"); // --> In Tuple types, Typescript will allowed us to push new data to the array, but it will throw an error if we try to push a new data with different type. Because in typescript push is exception for Tuple types
+exampleDataUsers.someFixedArray[1] = 10; // --> We can also change the value of the array
 console.log(exampleDataUsers.age);
 console.log(exampleDataUsers);
 for (var _i = 0, _a = exampleDataUsers.hobbies; _i < _a.length; _i++) {
--- 01 - Basics/javascripts/app.ts
+++ 01 - Basics/javascripts/app.ts
@@ -3,7 +3,8 @@ type IUsers = { // --> First, we define the type of the data we want to store
   age: number;
   country: string;
   hobbies: string[]; // --> We can also define arrays like this
-  someMixedArray: (string | number)[]; // --> We can also define arrays with mixed types like this
+  someMixedArray: (string | number)[]; // --> We can also define arrays with mixed types like this, and it also called as union types
+  someFixedArray: [string, number]; // --> We can also define arrays with fixed types and length like this, and it also called as Tuple
   isAlive: boolean;  
 }
 
@@ -13,9 +14,13 @@ const exampleDataUsers:IUsers = { // --> Then, we create a variable of that type
   country: "Indonesia",
   hobbies: ["Coding", "Reading", "Gaming"],
   someMixedArray: ["Coding", 18, "Indonesia"],
+  someFixedArray: ["Tuple Types", 2],
   isAlive: true
 }
 
+exampleDataUsers.someFixedArray.push("Rafi"); // --> In Tuple types, Typescript will allowed us to push new data to the array, but it will throw an error if we try to push a new data with different type. Because in typescript push is exception for Tuple types
+exampleDataUsers.someFixedArray[1] = 10 // --> We can also change the value of the array
+// exampleDataUsers.someFixedArray = ["Rafi", 18, 19] // --> But we can't change the length of the array, because it's fixed but you should remember for method push is exception for Tuple types
 console.log(exampleDataUsers.age)
 console.log(exampleDataUsers);
 
