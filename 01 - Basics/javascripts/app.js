4385e2145e5337737e0bcaaa774eee1420a313d2
Apiiyu <khoirulloh.rafi2@gmail.com>
2022-12-01 12:19:37 +0700

    Learning: Callbacks Function on Typescript

--- 01 - Basics/javascripts/app.js
+++ 01 - Basics/javascripts/app.js
@@ -18,5 +18,12 @@ var printResult = function (result) {
 var combineValues; // --> We can define function as types too. As example in this case we define function type, which takes two numbers and return number.
 combineValues = addNumber; // --> We can assign function to variable, which is defined as function type.
 // combineValues = printResult; // --> We can't assign function to variable, which is defined as function type, because this function doesn't return number.
+var addAndHandleNumbers = function (firstNumber, secondNumber, callback) {
+    var result = firstNumber + secondNumber;
+    callback(result); // --> We can call callback function and pass the result of add numbers.
+};
 printResult(addNumber(5, 12));
 console.log(combineValues(10, 10));
+addAndHandleNumbers(10, 20, function (result) {
+    console.log(result);
+});
--- 01 - Basics/javascripts/app.ts
+++ 01 - Basics/javascripts/app.ts
@@ -21,5 +21,13 @@ let combineValues: (number1: number, number2: number) => number; // --> We can d
 combineValues = addNumber; // --> We can assign function to variable, which is defined as function type.
 // combineValues = printResult; // --> We can't assign function to variable, which is defined as function type, because this function doesn't return number.
 
+const addAndHandleNumbers = (firstNumber: number, secondNumber: number, callback: (result: number) => void) => { // --> We can define callback function and it's types too. As example in this case we define callback function, which takes the result of add numbers and we also define types callback as void.
+  const result = firstNumber + secondNumber;
+  callback(result); // --> We can call callback function and pass the result of add numbers.
+}
+
 printResult(addNumber(5, 12));
 console.log(combineValues(10, 10));
+addAndHandleNumbers(10, 20, (result) => { // --> We can pass callback function as argument
+  console.log(result);
+})
